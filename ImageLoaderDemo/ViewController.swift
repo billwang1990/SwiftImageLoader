78551cb9fff796d21e0b332da967a8fa38cc7652
billwang1990 <billwang1990@gmail.com>
2015-06-30 11:43:40 +0800

    Initial Commit

---
+++ ImageLoaderDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageLoaderDemo
+//
+//  Created by wangyaqing on 15/6/30.
+//  Copyright (c) 2015年 billwang1990.github.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
